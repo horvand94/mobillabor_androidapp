005d9e93c5fbf265a7d13886a473f9e96ead0c3c
Horváth András <horvand10@gmail.com>
2019-04-07 13:44:28 +0200

    Dagger injector added.

--- app/src/main/java/com/example/mobillaborandroidapp/MobilLaborApplication.kt
+++ app/src/main/java/com/example/mobillaborandroidapp/MobilLaborApplication.kt
@@ -1,12 +1,13 @@
 package com.example.mobillaborandroidapp
 
 import android.app.Application
+import com.example.mobillaborandroidapp.ui.UIModule
 
 class MobilLaborApplication : Application() {
     lateinit var injector: MobilLaborApplicationComponent
 
     override fun onCreate() {
         super.onCreate()
-        //injector = DaggerMobilLaborApplicationComponent.builder().uIModule(UIModule(this)).build()
+        injector = DaggerMobilLaborApplicationComponent.builder().uIModule(UIModule(this)).build()
     }
 }
